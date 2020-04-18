32e5feec10bd9059da997f9fea12a7daa6f71c95
Cristobal <cristobal.becerraf@usach.cl>
2020-04-18 16:09:51 -0400

    agregado listener para detección facial

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 // Primero se obtiene el elemento video (se creó con la id webcam-video)
 const video = document.getElementById('webcam-video')
 
-
+// Promise para cargar webcam al cargar los modelos
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo)
 
 
 // Conexión a webcam
@@ -14,4 +20,12 @@ function startVideo() {
         )
 }
 
-startVideo()
+// Se agrega listener para obtener detecciones y dibujar sobre el video
+video.addEventListener('play', () => {
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        console.log(detections)
+    }, 100)
+})
+
+
